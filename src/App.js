779ce90af94ab4316188709fe9d1d51136e07a79
braybowler <braybowler1995@gmail.com>
2024-09-24 21:19:28 -0400

    fix public files

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/contact" element={<ContactPage/>}/>
-
             </Routes>
         </Router>
     );
