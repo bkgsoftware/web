39b6c72d446b677a72bfebce4e54caec4d20e65f
Karl Stencell <karldstencell@gmail.com>
2024-10-01 20:56:43 -0400

    header and hero first pass

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import ContactPage from './pages/ContactPage';
-import NavBar from "./components/NavBar";
-import AboutPage from "./pages/AboutPage";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LandingPage from './pages/LandingPage';
+// import ContactPage from './pages/ContactPage';
+// import NavBar from "./components/NavBar";
+// import AboutPage from "./pages/AboutPage";
+import Header from "./components/Header";
+import Hero from './components/Hero';
 
 function App() {
     return (
-        <Router>
-            <NavBar></NavBar>
+      // <Router>
+      //     <NavBar></NavBar>
 
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
-                <Route path="/contact" element={<ContactPage/>}/>
-            </Routes>
-        </Router>
+      //     <Routes>
+      //         <Route path="/" element={<LandingPage/>}/>
+      //         <Route path="/about" element={<AboutPage/>}/>
+      //         <Route path="/contact" element={<ContactPage/>}/>
+      //     </Routes>
+      // </Router>
+    //   <div className="min-h-screen flex flex-col">
+    //     <Header />
+    //     <main className="flex-grow">
+    //       <WelcomeSection />
+    //       <ServicesSection />
+    //     </main>
+    //     <Footer />
+    //   </div>
+    <div className="min-h-screen flex flex-col">
+        <Header/>
+        <Hero/>
+    </div>
     );
 }
 
