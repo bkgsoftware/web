e5b48753a5b9347f1afe2099fa963deac99ef8b0
Gavin Abeele <abeele.s.gavin@gmail.com>
2024-10-01 20:34:29 -0400

    refactor contact form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import ContactForm from '../forms/ContactForm'
+import React from "react";
+import ContactForm from "../forms/ContactForm";
 
 const ContactPage = () => {
     return (
-        <section>
-            <div className=''>
-                <h1 className='text-3xl'>Get in touch with us</h1>
-                <div class="bg-blue-500 w-12 mt-2 h-2 rounded-lg"></div>
-                <br></br>
-                <div className=''>
-                    <ContactForm />
-                </div>
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                />
             </div>
-        </section>
+            <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
+                <p className="mt-2 text-lg leading-8 text-gray-600">
+                    Thinking about your next project? Connect with us to learn more about how we can help you and your business. 
+                </p>
+            </div>
+            <ContactForm /> 
+        </div>
     );
 };
 
