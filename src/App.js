4d70e68977354bb5dfc94d032cf97d801d37aca7
Karl Stencell <karldstencell@gmail.com>
2024-10-07 21:32:02 -0400

    added router functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
-import Hero from './components/Hero';
-import TitleSection from './components/TitleSection';
-import FeatureSection from './components/FeatureSection';
-import FeatureSectionImage1 from './assets/featuresection1.png';
-import FeatureSectionImage2 from './assets/featuresection2.png';
+import HomePage from "./pages/HomePage";
+import ServicesPage from "./pages/ServicesPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
     return (
-    <div className="min-h-screen flex flex-col">
-        <Header/>
-        <Hero/>
-        <TitleSection title="Our Services" description="We offer a wide range of services to help your business succeed"/>
-        <FeatureSection title="Web Development" description="We build websites that are fast, secure, and easy to use maybe" imageSrc={FeatureSectionImage1} />
-        <FeatureSection title="User Design" description="Something about unique design for their yadda yadda yadda" imageSrc={FeatureSectionImage2} reverse='True' />
-    </div>
+        <BrowserRouter>
+            <div className="min-h-screen flex flex-col">
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/services" element={<ServicesPage/>}/>
+                    <Route path="/about" element={<AboutPage/>}/>
+                    <Route path="/contact" element={<ContactPage/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Menu, X } from 'lucide-react'
+import { Link } from "react-router-dom";
 import logo from "../assets/bkg.svg";
 
 const navigation = [
     { name: 'Home', href: '/' },
-    { name: 'Services', href: '/' },
-    { name: 'Meet the Team', href: '/' },
-    { name: 'Contact Us', href: '/' },
+    { name: 'Services', href: '/services' },
+    { name: 'Meet the Team', href: '/about' },
+    { name: 'Contact Us', href: '/contact' },
   ]
 
 const Header = () => {
@@ -17,15 +18,15 @@ const Header = () => {
     <header className="absolute inset-x-0 top-0 z-50 bg-white text-black">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 max-w-screen-xl mx-auto">
           <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link to="/" className="flex -m-1.5 p-1.5">
               <span className="sr-only">BKG Software</span>
               <img
                 alt="Logo"
                 src={logo}
                 className="h-8 w-auto"
               />
-            </a>
-            <h1 className="flex items-center ml-5">BKG Software</h1>
+              <h1 className="flex items-center ml-5">BKG Software</h1>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -39,9 +40,9 @@ const Header = () => {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href}>
+              <Link key={item.name} to={item.href}>
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
         </nav>
@@ -49,14 +50,14 @@ const Header = () => {
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-black sm:ring-opacity-30">
             <div className="flex items-center justify-between">
-              <a href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+              <Link to="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">BKG Software</span>
                 <img
-                  alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="Logo"
+                  src={logo}
                   className="h-8 w-auto"
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
@@ -70,22 +71,14 @@ const Header = () => {
               <div className="-my-6 divide-y divide-black divide-opacity-30">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
-                </div>
-                <div className="py-6">
-                  <a
-                    href="/"
-                    className="-mx-3 block rounded-lg px-3 py-2.5"
-                  >
-                    Log in
-                  </a>
                 </div>
               </div>
             </div>
