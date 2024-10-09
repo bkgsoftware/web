d16bc5af8e56edba3b16e289de392376b7dc25b9
Karl Stencell <karldstencell@gmail.com>
2024-10-08 20:00:21 -0400

    sticckkky

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50 bg-white text-black">
+    <header className="sticky inset-x-0 top-0 z-50 bg-white text-black">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 max-w-screen-xl mx-auto">
           <div className="flex lg:flex-1">
             <Link to="/" className="flex -m-1.5 p-1.5">
