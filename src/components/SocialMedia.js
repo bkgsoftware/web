735b55595d69ad932549d7679e40b7e5f03239a3
braybowler <braybowler1995@gmail.com>
2024-10-15 20:26:11 -0400

    style changes

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,7 +5,7 @@ const Copyright = () => {
     return (
         <nav className="flex items-center justify-center text-center p-2 pl-10 pr-10 text-white bg-black">
             <a href="https://github.com/bkgsoftware">
-                <Github color='white'></Github>
+                <Github color='white' className="transition duration-350 hover:scale-110"></Github>
             </a>
         </nav>
     );
