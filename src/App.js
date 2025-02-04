72b5f6f807fe14fa2a4dfeee2d961bdc0e8ff694
braybowler <braybowler1995@gmail.com>
2025-02-04 18:13:57 -0500

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import ServicesPage from "./pages/ServicesPage";
-import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import FooterBar from "./components/FooterBar";
 
