303e23c2fb443d47e6bac9f0a3c4f9833fdb144e
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-02-04 20:46:13 -0500

    remove clutter

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import QuickContact from "./QuickContact";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const FooterBar = () => {
   const location = useLocation();
@@ -20,22 +20,11 @@ const FooterBar = () => {
       {showHtml ? <QuickContact /> : ""}
       <div>
         <div className="max-w-7xl mx-auto px-4 py-6">
-          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+          <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0">
             <p className="text-sm">
               &copy; {currentYear} BKG Software. All rights reserved.
             </p>
-            <div className="flex space-x-6">
-              {/* TODO Can be a quick generation of privacy and stuff. Esp since we do collected emails */}
-              <Link to="#" className="text-sm hover:text-white">
-                Privacy Policy
-              </Link>
-              <Link to="#" className="text-sm hover:text-white">
-                Terms of Use
-              </Link>
-              <Link to="#" className="text-sm hover:text-white">
-                Cookie Policy
-              </Link>
-            </div>
+
           </div>
         </div>
       </div>
