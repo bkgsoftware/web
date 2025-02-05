8605e4586e2fd5f0de1e2a71616a221a23714d87
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-02-04 20:33:20 -0500

    swapping contact to quick-contact

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ContactForm from "../forms/ContactForm";
 import {Link} from "react-router-dom";
+import QuickContact from "../components/QuickContact";
 
 const ContactPage = () => {
     return (
-        <div className="isolate  px-6 py-24 sm:py-32 lg:px-8 min-h-[calc(100vh-10rem)]">
-            <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
-                    Thinking about your next project? Connect with us to learn more about how we can help you and your business. Get in touch via the contact form, or send us an email directly to <Link to="mailto:consulting@bkgsoftware.com" className="text-orange">consulting@bkgsoftware.com</Link>
-                </p>
-            </div>
-            <ContactForm />
+        <div className="isolate  px-6 py-24 sm:py-32 lg:px-8 ">
+            <QuickContact />
         </div>
     );
 };
