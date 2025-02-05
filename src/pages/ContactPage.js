3a0c1abfcaddfef2d594f8601524db03ecacbafd
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-02-04 20:04:55 -0500

    add min height to contact us

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 
 const ContactPage = () => {
     return (
-        <div className="isolate  px-6 py-24 sm:py-32 lg:px-8">
+        <div className="isolate  px-6 py-24 sm:py-32 lg:px-8 min-h-[calc(100vh-10rem)]">
             <div className="mx-auto max-w-2xl text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
