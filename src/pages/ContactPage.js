5bf83cb2a4b5a3be3fef21d89191e551d5fb47cd
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-02-04 20:42:50 -0500

    fix form showing when menu is open

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,7 @@ import QuickContact from "../components/QuickContact";
 
 const ContactPage = () => {
     return (
-        <div className="isolate  px-6 py-24 sm:py-32 lg:px-8 ">
+        <div className="px-6 py-24 sm:py-32 lg:px-8 ">
             <QuickContact />
         </div>
     );
